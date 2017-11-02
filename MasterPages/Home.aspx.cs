f9149cc9f52f8133f853367e4b380e41c816008d
butnaruandrei <butnaruandreimadalin@gmail.com>
2017-11-02 21:44:46 +0200

    Initial commit

---
+++ MasterPages/Home.aspx.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+public partial class Home : System.Web.UI.Page
+{
+}
---
+++ MasterPages/MasterPage.master.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+namespace MasterPages
+{
+    public partial class MasterPage : System.Web.UI.MasterPage
+    {
+    }
+}
